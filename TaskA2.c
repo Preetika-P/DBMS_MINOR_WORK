dad74fc76a9a25430bace644c98546e460bbb194
Preetika-P <147710881+Preetika-P@users.noreply.github.com>
2024-10-08 23:41:28 +0530

    Update TaskA2.c

--- TaskA2.c
+++ TaskA2.c
@@ -4,25 +4,25 @@ int main() {
     int arr[6] = {10, 20, 30, 40, 50}; 
     int size = 5;
 
-    // Display the original array
+    // Display 
     printf("Original array:\n");
     for (int i = 0; i < size; i++) {
         printf("Element at index %d: %d\n", i, arr[i]);
     }
 
-    //Update the element at index 2
+    //Update 
     arr[2] = 99;
     printf("\nAfter modifying element at index 2:\n");
     for (int i = 0; i < size; i++) {
         printf("Element at index %d: %d\n", i, arr[i]);
     }
 
-    //Insert 60 at index 3 (Shift elements)
+    //Insert 60 at index 3
     int indexToInsert = 3;
     int newValue = 60;
 
     for (int i = size; i > indexToInsert; i--) {
-        arr[i] = arr[i - 1]; // Shift elements to the right
+        arr[i] = arr[i - 1]; 
     }
     arr[indexToInsert] = newValue;
     size++;
@@ -32,10 +32,10 @@ int main() {
         printf("Element at index %d: %d\n", i, arr[i]);
     }
 
-    // Remove the element at index 1 (Shift elements)
+    // Remove the element at index 1
     int indexToDelete = 1;
     for (int i = indexToDelete; i < size - 1; i++) {
-        arr[i] = arr[i + 1]; // Shift elements to the left
+        arr[i] = arr[i + 1]; 
     }
     size--; 
 
@@ -44,7 +44,6 @@ int main() {
         printf("Element at index %d: %d\n", i, arr[i]);
     }
 
-    //Find the element at a specific index 
     int indexToSearch = 2;
     printf("\nElement at index %d: %d\n", indexToSearch, arr[indexToSearch]);
 
